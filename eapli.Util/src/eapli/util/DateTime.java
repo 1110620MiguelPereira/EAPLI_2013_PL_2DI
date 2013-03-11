3da3eb132c81225a40b108d187a7f8609b14b0d8
PauloGandraSousa <pag@isep.ipp.pt>
2013-03-11 22:17:10 +0000

    stop using CalendarDate and Date

--- eapli.Util/src/eapli/util/DateTime.java
+++ eapli.Util/src/eapli/util/DateTime.java
@@ -4,8 +4,13 @@
  */
 package eapli.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import sun.util.calendar.CalendarDate;
 import sun.util.calendar.CalendarSystem;
 
@@ -14,41 +19,53 @@ import sun.util.calendar.CalendarSystem;
  * @author Paulo Gandra Sousa
  */
 public class DateTime {
-        
-    public static CalendarDate today() {
-        CalendarSystem calendar = CalendarSystem.getGregorianCalendar();
-        CalendarDate today = calendar.getCalendarDate();
+
+    public static Calendar today() {
+        Calendar today = new GregorianCalendar();
         return today;
     }
-    
-    public static int weekNumber(CalendarDate date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(date.getMillis());
-        return calendar.get(Calendar.WEEK_OF_YEAR);
+
+    public static int weekNumber(Calendar date) {
+        return date.get(Calendar.WEEK_OF_YEAR);
     }
 
     public static int weekNumber(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(date.getTime());
-        return calendar.get(Calendar.WEEK_OF_YEAR);
+        return weekNumber(dateToCalendar(date));
     }
-    
-    public static int currentWeekNumber() {       
+
+    public static Calendar dateToCalendar(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal;
+    }
+
+    public static int currentWeekNumber() {
         return weekNumber(today());
     }
-    
-    public static CalendarDate newCalendarDate(int year, int month, int day) {
-        CalendarSystem calendar = CalendarSystem.getGregorianCalendar();
-        CalendarDate date = calendar.getCalendarDate();
-        date.setYear(year);
-        date.setMonth(month);
-        date.setDayOfMonth(day);
+
+    public static Calendar newCalendarDate(int year, int month, int day) {
+        Calendar date = new GregorianCalendar(year, month, day);
         return date;
     }
-    
+
     public static Date newDate(int year, int month, int day) {
         Calendar c = Calendar.getInstance();
-        c.set(year, month-1, day);
+        c.set(year, month - 1, day);
         return c.getTime();
+    }
+
+    public static Date parseDate(String aDateString, String format) {
+        try {
+            SimpleDateFormat df = new SimpleDateFormat(format);
+            Date date = df.parse(aDateString);
+            return date;
+        } catch (ParseException ex) {
+            Logger.getLogger(Console.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public static Date parseDate(String aDateString) {
+        return parseDate(aDateString, "dd-MM-yyyy");
     }
 }
