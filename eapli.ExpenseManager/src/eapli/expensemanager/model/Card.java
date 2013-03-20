88493c2e063c00b64a1a1072c47d3337f2fafad3
PedroBranco <branco_tico@hotmail.com>
2013-03-20 19:55:09 +0000

    created class Card

---
+++ eapli.ExpenseManager/src/eapli/expensemanager/model/Card.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.expensemanager.model;
+
+/**
+ *
+ * @author Pedro
+ */
+public class Card {
+    
+    int number;
+    String bankName;
+    
+    public Card(){}
+    
+    public Card(int number, String bankName){ this.number = number; this.bankName=bankName;}
+    
+    
+}
