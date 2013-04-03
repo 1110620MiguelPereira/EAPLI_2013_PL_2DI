a13c6159a6c85b10beb9dded867ad924de005d65
joelalves <joel.17.alves@gmail.com>
2013-04-03 14:08:57 +0100

    Listagem das despesas(sem data)
    1110498
    1110755
    1110773

--- eapli.ExpenseManager/src/eapli/expensemanager/controllers/ExpenseVisualizationController.java
+++ eapli.ExpenseManager/src/eapli/expensemanager/controllers/ExpenseVisualizationController.java
@@ -4,10 +4,22 @@
  */
 package eapli.expensemanager.controllers;
 
+import eapli.expensemanager.model.Expense;
+import eapli.expensemanager.repositories.ExpenseRepository;
+import eapli.expensemanager.repositories.RepositoryFactory;
+import java.util.List;
+
 /**
  *
  * @author Big-D2xl
  */
 public class ExpenseVisualizationController extends BaseController{
+
+    public List<Expense> ExpenseVisualization() {
+        
+        ExpenseRepository repo= RepositoryFactory.instance().getExpenseRepository();
+         //To change body of generated methods, choose Tools | Templates.
+        return repo.getListExpenses();
+    }
     
 }
