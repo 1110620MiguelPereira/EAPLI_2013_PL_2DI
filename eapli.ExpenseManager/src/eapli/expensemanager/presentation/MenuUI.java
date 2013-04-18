aefb2d1c71ef5e141876c5da26d260049d9fc50a
Antonio Vale <antoniomfvale@hotmail.com>
2013-04-18 14:41:45 +0100

    Insert register income option in MenuUI(Correction of merge error)

--- eapli.ExpenseManager/src/eapli/expensemanager/presentation/MenuUI.java
+++ eapli.ExpenseManager/src/eapli/expensemanager/presentation/MenuUI.java
@@ -17,19 +17,8 @@ public class MenuUI{
         do {
        
             System.out.println("EXPENSE MANAGER");
-<<<<<<< HEAD
-            System.out.println("1. Register expense");
-            System.out.println("2. Register expense type");
-            System.out.println("3. Register payment method");
-            System.out.println("4. List monthly expenses");
-            System.out.println("5. List expenses types");
-            System.out.println("6. Register income type");
-            System.out.println("7. List income types");
-            System.out.println("8. Register income");
-=======
             System.out.println("1. List");
             System.out.println("2. Register");
->>>>>>> 81bf7e9f41f8f4fb4a9a830566ce1501058e9065
             System.out.println("0. Exit");
             
             option = Console.readIntegerFromConsole("Insert option:");
@@ -66,40 +55,12 @@ public class MenuUI{
                     }
                     break;
                 case 2:
-<<<<<<< HEAD
-                    RegisterExpenseTypeUI tui = new RegisterExpenseTypeUI();
-                    tui.show();
-                    break;
-                case 3:
-                    RegisterPaymentMethodUI paymentMethodUI = new RegisterPaymentMethodUI();
-                    paymentMethodUI.show();
-                    break;
-                case 4:
-                    ExpenseVisualizationUI tuii=new ExpenseVisualizationUI();
-                    tuii.show();
-                    break;
-                case 5:
-                    ListExpenseTypeUI letUI = new ListExpenseTypeUI();
-                    letUI.show();
-                    break;
-                case 6:
-                    RegisterIncomeTypeUI incTypeUI = new RegisterIncomeTypeUI();
-                    incTypeUI.show();
-                    break;
-                case 7:
-                    ListIncomeTypeUI listIncUI = new ListIncomeTypeUI();
-                    listIncUI.show();
-                    break;
-                case 8:
-                    RegisterIncomeUI ri = new RegisterIncomeUI();
-                    ri.show();
-                    break;
-=======
                     System.out.println("Register:");
                     System.out.println("1. Expense");
                     System.out.println("2. Expense type");
                     System.out.println("3. Payment method");
-                    System.out.println("4. Income type");
+                    System.out.println("4. Income");
+                    System.out.println("5. Income type");
                     System.out.println("0. Exit");
                     option = Console.readIntegerFromConsole("Insert option:");
                     switch(option){
@@ -118,11 +79,14 @@ public class MenuUI{
                             paymentMethodUI.show();
                             break;
                         case 4:
+                            RegisterIncomeUI incUI = new RegisterIncomeUI();
+                            incUI.show();
+                            break;
+                        case 5:
                             RegisterIncomeTypeUI incTypeUI = new RegisterIncomeTypeUI();
                             incTypeUI.show();
                             break;
-                    }     
->>>>>>> 81bf7e9f41f8f4fb4a9a830566ce1501058e9065
+                    }
             }
         } while (option != 0);
     }
