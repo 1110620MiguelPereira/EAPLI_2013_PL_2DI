4e457f5ae139ebb97a1358523a87803f252b6769
PauloGandraSousa <pag@isep.ipp.pt>
2013-04-23 15:55:46 +0000

    added empty  JPA util class

---
+++ eapli.ExpenseManager/src/eapli/expensemanager/repositories/hibernate/JpaHibernateUtil.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.expensemanager.repositories.hibernate;
+
+/**
+ *
+ * @author Paulo Gandra Sousa
+ * http://stackoverflow.com/questions/3888575/single-dao-generic-crud-methods-jpa-hibernate-spring
+ * and on https://burtbeckwith.com/blog/?p=40
+ */
+public class JpaHibernateUtil {
+    
+}
