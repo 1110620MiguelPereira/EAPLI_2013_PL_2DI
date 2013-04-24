7d711882cf7911e8398550c17ac527dd3ffde8a2
Marcel Schmitz <marcel.schmitz@mac.com>
2013-04-24 14:15:39 +0100

    created JpaHibernateUtil + HibernateExpenseTypeRepository
    1110998 Marcel Schmitz
    1111224 Luís Ferreira

---
+++ eapli.ExpenseManager/src/eapli/expensemanager/repositories/hibernate/HibernateExpenseTypeRepository.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.expensemanager.repositories.hibernate;
+
+import eapli.expensemanager.model.ExpenseType;
+import eapli.expensemanager.model.MovementType;
+
+import eapli.expensemanager.repositories.ExpenseTypeRepository;
+import eapli.expensemanager.repositories.hibernate.*;
+import java.util.List;
+
+/**
+ *
+ * @author schmitzoide
+ */
+public class HibernateExpenseTypeRepository extends JpaHibernateUtil<ExpenseType> implements ExpenseTypeRepository {
+
+    // VAMOS AQUI
+
+    
+    public HibernateExpenseTypeRepository(){
+    }
+    
+    @Override
+    public void save(ExpenseType expenseType) {
+        update(expenseType);
+    }
+
+    @Override
+    public List<ExpenseType> getAll() {
+        return (List)findAll();
+    }
+    
+}
