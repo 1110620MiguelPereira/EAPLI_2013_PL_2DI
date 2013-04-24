d8e9abf887e5029aa497d5276cfe8c04bdbf254f
Cristiano Silva <cristiano_ac_silva@hotmail.com>
2013-04-24 14:37:37 +0100

    finished List Expenses UC 6A 1110755 1110773 1110498

--- eapli.ExpenseManager/src/eapli/expensemanager/controllers/ExpenseVisualizationController.java
+++ eapli.ExpenseManager/src/eapli/expensemanager/controllers/ExpenseVisualizationController.java
@@ -7,6 +7,7 @@ package eapli.expensemanager.controllers;
 import eapli.expensemanager.model.Expense;
 import eapli.expensemanager.repositories.ExpenseRepository;
 import eapli.expensemanager.repositories.RepositoryFactory;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -15,11 +16,11 @@ import java.util.List;
  */
 public class ExpenseVisualizationController extends BaseController{
 
-    public List<Expense> getExpensesOfMonth(int month,int year) {
+    public List<Expense> getExpensesOfMonth(Calendar initDate,Calendar finalDate) {
         
         ExpenseRepository repo= RepositoryFactory.instance().getExpenseRepository();
 
-        return repo.getListExpenses(month,year);
+        return repo.getListExpenses(initDate,finalDate);
     }
     
 }
