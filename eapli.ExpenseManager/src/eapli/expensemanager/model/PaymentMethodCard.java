30d6d420775d5ac118441732ea63e199465388c1
1110634 <1110634@isep.ipp.pt>
2013-05-22 14:10:49 +0100

    Added the type of payment method when they are listed
    1110634 Joao Ramos
    1110690 Hugo Silva

--- eapli.ExpenseManager/src/eapli/expensemanager/model/PaymentMethodCard.java
+++ eapli.ExpenseManager/src/eapli/expensemanager/model/PaymentMethodCard.java
@@ -35,6 +35,6 @@ public abstract class PaymentMethodCard extends PaymentMethod implements java.io
 
     @Override
     public String toString() {
-        return "\n"+super.toString()+"Card number: " + number + "\nbank name: " + bankName;
+        return "\n"+super.toString()+"Card number: " + number + "\nbank name: " + bankName+"\n";
     }
 }
